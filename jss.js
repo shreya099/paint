9e901328508a4ae16a339b9d6c58779b8d648cd3
Shreya jain <75029391+shreya099@users.noreply.github.com>
2021-09-22 20:57:19 +0530

    Update jss.js

--- jss.js
+++ jss.js
@@ -1,7 +1,7 @@
 let canvas = document.getElementById("canvas");
 
 canvas.width = window.innerWidth - 60;
-canvas.height = 400;
+canvas.height = window.innerHeight * 0.6;
 let context = canvas.getContext("2d");
 let start_bg="white";
 context.fillStyle = start_bg;
