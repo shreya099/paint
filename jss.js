958be0e148579a88b07808dbc1c118a9fe040138
Shreya jain <75029391+shreya099@users.noreply.github.com>
2021-09-22 20:55:23 +0530

    Update jss.js

--- jss.js
+++ jss.js
@@ -1,95 +1,7 @@
- //  PROJECT1-bg color 
-// const b=document.querySelector('.btn');
-// const bodybg=document.querySelector('body');
-// const color=['green','yellow','pink','#09efef']
-
-// b.addEventListener('click',chng);
-// function chng(min,max) {
-// 	// body...
-
-//     let r= Math.floor(Math.random() * color.length);
-// 	bodybg.style.backgroundColor=color[r];
-// }
-
-
- //  PROJECT2-hex color 
-// const hex=[0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
-//  const b=document.querySelector('.btn');
-//  const bodybg=document.querySelector('body');
-//   const h=document.querySelector('.hex');
-//   b.addEventListener('click',gethex);
-//   function gethex() {
-//   	// body...
-  
-//   	let hexval='#';
-//   	for(let i=0;i<6;i++)
-//   	{
-//   			let r=Math.floor(Math.random()* hex.length);
-//    hexval+=hex[r];
-//   	}
-//   	bodybg.style.backgroundColor=hexval;
-//   	h.innerHTML=hexval;
-//   }
-
-
-
- //  PROJECT3-quote  
-
-// const quote=[
-// {
-// 	name:'shreya',
-// 	quote:'hyy'
-// },
-// {
-// 	name:'shreya0',
-// 	quote:'hyy'
-// },
-// {
-// 	name:'shreya',
-// 	quote:'hy'
-// },
-// ]
-
-// const b=document.querySelector('.btn');
-// const q=document.querySelector('#quote');
-// const a=document.querySelector('#author');
-//   b.addEventListener('click',display);
-
-//   function display() {
-//   	// body...
-//   	let r=Math.floor(Math.random()* quote.length);
-//   	q.innerHTML=quote[r].quote;
-//   	a.innerHTML=quote[r].name;
-//   }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 let canvas = document.getElementById("canvas");
 
 canvas.width = window.innerWidth - 60;
-canvas.height = 400;
+canvas.height = window.innerHeight * 0.6;
 let context = canvas.getContext("2d");
 let start_bg="white";
 context.fillStyle = start_bg;
@@ -105,13 +17,7 @@ let draw_width = "2";
 let is_drawing = false;
 
 
-canvas.addEventListener("touchstart", start, false);
-canvas.addEventListener("touchmove", draw, false);
-canvas.addEventListener("touchend", stop, false);
-canvas.addEventListener("mousedown", start, false);
-canvas.addEventListener("mousemove", draw, false);
-canvas.addEventListener("mouseup", stop, false);
-canvas.addEventListener("mouseout", stop, false);
+
 
 function change_color(element) {
   draw_color = element.style.background;
@@ -175,16 +81,6 @@ function draw(event) {
   }
   event.preventDefault();
 }
-function getX(event) {
-  if (event.pageX == undefined) {return event.targetTouches[0].pageX - canvas.offsetLeft}
-  else {return event.pageX - canvas.offsetLeft}
-  }
-
-
-function getY(event) {
-  if (event.pageY == undefined) {return event.targetTouches[0].pageY - canvas.offsetTop}
-  else {return event.pageY - canvas.offsetTop}
-}
 
 function stop(event) {
   if (is_drawing) {
@@ -198,8 +94,25 @@ function stop(event) {
 
  
 }
+function getX(event) {
+  if (event.pageX == undefined) {return event.targetTouches[0].pageX - canvas.offsetLeft}
+  else {return event.pageX - canvas.offsetLeft}
+  }
 
 
+function getY(event) {
+  if (event.pageY == undefined) {return event.targetTouches[0].pageY - canvas.offsetTop}
+  else {return event.pageY - canvas.offsetTop}
+}
+
+
+canvas.addEventListener("touchstart", start, false);
+canvas.addEventListener("touchmove", draw, false);
+canvas.addEventListener("touchend", stop, false);
+canvas.addEventListener("mousedown", start, false);
+canvas.addEventListener("mousemove", draw, false);
+canvas.addEventListener("mouseup", stop, false);
+canvas.addEventListener("mouseout", stop, false);
 
 
 function Restore() {
